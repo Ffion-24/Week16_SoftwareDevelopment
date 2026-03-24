ad813e69d7d2553d78c57b908af9f48aca7e4b60
Ffion-24 <2504455@leedstrinity.ac.uk>
2026-03-24 09:46:41 +0000

    binary Search

---
+++ searching.py
@@ -0,0 +1,28 @@
+data = [2, 5, 7, 9, 12, 15, 18, 21]
+
+def binary_search(arr, target):
+    lo, hi = 0, len(arr) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if arr[mid] == target:
+            return mid
+        if target < arr[mid]:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
+
+def binary_search_with_counts(arr, target):
+    lo, hi = 0, len(arr) - 1
+    comparisons = 0
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        comparisons += 1
+        if arr[mid] == target:
+            return mid, comparisons
+        comparisons += 1
+        if target < arr[mid]:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1, comparisons
